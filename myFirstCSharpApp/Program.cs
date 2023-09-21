c23d906f73d38061217957d963469cdff4249193
kosmonafte <itrzn@mail.ru>
2023-09-21 16:51:29 +0300

    CSharpHW

---
+++ myFirstCSharpApp/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using System.Data;
+
+int a, b, c, x, y;
+string str;
+Console.Write("Введите сторону A: ");
+str = Console.ReadLine();
+a = Convert.ToInt32(str);
+Console.Write("Введите сторону B: ");
+str = Console.ReadLine();
+b = Convert.ToInt32(str);
+Console.Write("Введите сторону C: ");
+str = Console.ReadLine();
+c = Convert.ToInt32(str);
+
+x = b / c;
+y = a / c;
+
+if (x == 0 || y == 0) {
+    str = "Нет квадратов";
+} else {
+    str = string.Format("В прямоугольник вмещается {0} квадратов, не занятая площадь {1}", x * y, (a * b - c * c * x * y));
+}
+
+Console.WriteLine(str);
